4d0d662015e0ada50a8f489bd9a52cbad662a0de
Filip <ilaka69@seznam.cz>
2023-09-21 19:43:34 +0200

    Changed ts config

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -5,7 +5,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST() {
   try {
