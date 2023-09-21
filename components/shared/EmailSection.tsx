96bdc1ce0f703ea611dc23149b9408bdbcb9a72b
Filip <ilaka69@seznam.cz>
2023-09-21 18:06:49 +0200

    Removed text

--- components/shared/EmailSection.tsx
+++ components/shared/EmailSection.tsx
@@ -31,7 +31,7 @@ const EmailSection = () => {
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-6 relative">
       {/* <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-primary-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2" /> */}
       <div className="z-10">
-        <h5 className="text-4xl font-bold text-white my-2 ">Let's connect</h5>
+        <h5 className="text-4xl font-bold text-white my-2 ">Lets connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
           I am currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I will try my
           best to get back to you!
