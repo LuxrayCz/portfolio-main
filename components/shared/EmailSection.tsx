8e20977f8001fe57b5d42bb010281a81dba0742a
Filip <ilaka69@seznam.cz>
2023-09-21 15:04:52 +0200

    Fixed bugs

--- components/shared/EmailSection.tsx
+++ components/shared/EmailSection.tsx
@@ -33,7 +33,7 @@ const EmailSection = () => {
       <div className="z-10">
         <h5 className="text-4xl font-bold text-white my-2 ">Let's connect</h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md">
-          I&apos;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&apos;ll try
+          I&#39;m currently looking for new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I&#39;ll try
           my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
